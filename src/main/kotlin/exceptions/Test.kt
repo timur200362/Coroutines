dd81f6b6aade04b49abe757ef6fb202fff3e9e53
Timur <timurka.askarov@inbox.ru>
2025-01-21 05:34:34 +0300

    8.13 Исключения. Часть 1

---
+++ src/main/kotlin/exceptions/Test.kt
@@ -0,0 +1,29 @@
+package org.example.exceptions
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+
+private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
+    println("Exception caught")
+}
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val scope = CoroutineScope(dispatcher + CoroutineName("name") + Job() + exceptionHandler)
+
+fun main() {
+    scope.launch {
+        method()
+    }
+    scope.launch {
+        method2()
+    }
+}
+
+suspend fun method() {
+    delay(3000)
+    error("")
+}
+
+suspend fun method2() {
+    delay(5000)
+    println("Method2 is finished")
+}
